1f4b371ed5a3d0943ca04490b59c56ff90e23d96
Adrian Statescu <mergesortv@gmail.com>
2025-01-06 00:07:27 +0200

    commit

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -5,12 +5,9 @@ const Home = () => {
     <>
       <div className="hero border-1 pb-3">
         <div className="card bg-dark text-white border-0 mx-3">
-          <img
-            className="card-img img-fluid"
-            src="./assets/main.png.jpg"
-            alt="Card"
-            height={500}
-          />
+        
+          <img className="card-img img-fluid" src="/assets/photo.png" alt="Card" height={500} />
+          
           <div className="card-img-overlay d-flex align-items-center">
             <div className="container">
               <h5 className="card-title fs-1 text fw-lighter">React Library</h5>
--- src/index.js
+++ src/index.js
@@ -28,6 +28,7 @@ root.render(
       <Provider store={store}>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/react-e-commerce" element={<Home />} />
           <Route path="/product" element={<Products />} />
           <Route path="/product/:id" element={<Product />} />
           <Route path="/about" element={<AboutPage />} />
