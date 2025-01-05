bac52b4696e10b8c1e06881e782a851fb1282570
Adrian Statescu <mergesortv@gmail.com>
2025-01-05 22:53:42 +0200

    commit

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -13,11 +13,9 @@ const Home = () => {
           />
           <div className="card-img-overlay d-flex align-items-center">
             <div className="container">
-              <h5 className="card-title fs-1 text fw-lighter">New Season Arrivals</h5>
+              <h5 className="card-title fs-1 text fw-lighter">React Library</h5>
               <p className="card-text fs-5 d-none d-sm-block ">
-                This is a wider card with supporting text below as a natural
-                lead-in to additional content. This content is a little bit
-                longer.
+              jQuery: A JavaScript library that is fast, small, and rich in features.
               </p>
             </div>
           </div>
